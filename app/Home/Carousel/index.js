385c3791e11138afcfda06eec5f38a6580ddb6fa
Jose Arce <jose.arce.dev@gmail.com>
2019-07-22 16:04:26 -0500

    Improve carousel

--- app/Home/Carousel/index.js
+++ app/Home/Carousel/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import { StyleSheet, View, Text} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
-import Swiper from './Swiper';
+class AntsCarousel extends React.PureComponent{
 
-const AntsCarousel = ({data = []}) => {
-
-  const items = data.map((ant) => {
+  renderItem = ({item}) => {
     return (
-      <View style={styles.slide1} key={ant.name}>
-        <Text style={styles.text}>{ant.name}</Text>
-        <Text style={styles.text}>{ant.odds}</Text>
+      <View style={styles.slide1} key={item.name}>
+        <Text style={styles.image}>{'🐜'}</Text>
+        <Text style={styles.text} numberOfLines={2} ellipsizeMode={'tail'}>{item.name}</Text>
+        <Text style={styles.text}>{`${item.odds}%`}</Text>
       </View>
     )
-  });
+  };
 
-  const lastPosition = data.length - 1;
-
-  return (
-    <View style={styles.wrapper}>
-      <Swiper
-        autoplay={true}
-        index={lastPosition}
-        loop={true}
-        showsPagination={false}>
-        {items}
-      </Swiper>
-    </View>
-  )
+  render () {
+    const {data} = this.props;
+    return (
+      <View style={styles.wrapper}>
+        <Carousel
+          layout={'default'}
+          data={data}
+          loop={true}
+          firstItem={data.length - 2}
+          loopClonesPerSide={10}
+          renderItem={this.renderItem}
+          sliderWidth={350}
+          itemWidth={120}
+        />
+      </View>
+    )
+  }
 }
 
 export default AntsCarousel;
@@ -38,10 +42,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
+    padding: 10,
   },
   text: {
     color: '#fff',
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: 'bold'
   },
+  image: {
+    fontSize: 30,
+    marginBottom: 10,
+  }
 });
